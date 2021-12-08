0f05da6bdcea3bf2d4ba416849706ecd30c13086
Frank-Proulx <frankproulx7@gmail.com>
2021-12-08 09:30:29 -0800

    add second test and get passing result
    Co-authored-by: Isaac Overstreet <isaacoverstreet3@gmail.com>
    Co-Authored-by: Marisa Edgar <marisaedgar1212@gmail.com>
    Co-authored-by: Matt Casperson <mc.casperson@gmail.com>

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -2,6 +2,8 @@ class RPS
   def wins?(player1, player2)
     if (player1 == "rock") && (player2 == "scissors")
       return true
+    elsif (player1 == "scissors") && (player2 == "paper")
+      return true
     end
   end
 end
