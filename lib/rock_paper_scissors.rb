52a9e7aa6516c8429069c444b143f5570cc86dfc
Frank-Proulx <frankproulx7@gmail.com>
2021-12-08 09:23:05 -0800

    add method and get first test pass
    Co-authored-by: Isaac Overstreet <isaacoverstreet3@gmail.com>
    Co-Authored-by: Marisa Edgar <marisaedgar1212@gmail.com>
    Co-authored-by: Matt Casperson <mc.casperson@gmail.com>

---
+++ lib/rock_paper_scissors.rb
@@ -0,0 +1,7 @@
+class RPS
+  def wins?(player1, player2)
+    if (player1 == "rock") && (player2 == "scissors")
+      return true
+    end
+  end
+end
---
+++ rock_paper_scissors_script.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/ruby
+
+require('./lib/rock_paper_scissors')
